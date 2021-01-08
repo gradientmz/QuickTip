8d25f076ca2962db3d87c6b2422ad82d2571bc13
Xinyun Zhou <xinyunzhouu@gmail.com>
2021-01-08 00:33:34 -0500

    Initial Commit

---
+++ QuickTip/QuickTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTipApp.swift
+//  QuickTip
+//
+//  Created by Mark Zhou on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
